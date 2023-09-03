bab71ab05d24adfbc4949a2de3c43833dbe62c36
Yago <yago.pereira@estudante.ufjf.br>
2023-09-02 23:20:47 -0300

    Começando o projeto Node: criando minha própria biblioteca.

---
+++ index.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto));
+    });
+}
+
+pegaArquivo('./arquivos/texto.md');
