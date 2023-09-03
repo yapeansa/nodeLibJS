99b1a5c0024b9adc4342932a68c1e619c457f5fe
Yago <yago.pereira@estudante.ufjf.br>
2023-09-03 12:26:48 -0300

    Lidando com assincronicidade.

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
+// Uma das formas de lidar com assincronicidade em Javascript
+
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    });
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then(texto => console.log(chalk.green(texto)))
+        .catch(trataErro);
 }
+
+// Um código síncrono
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     });
+// }
 
 pegaArquivo('./arquivos/texto.md');
