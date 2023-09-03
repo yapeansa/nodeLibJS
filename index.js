19ca9ecc77dedfe289710fb35cce6a48cedbb9c1
Yago <yago.pereira@estudante.ufjf.br>
2023-09-03 13:05:08 -0300

    Async/Await

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-// Uma das formas de lidar com assincronicidade em Javascript
+// async/await
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then(texto => console.log(chalk.green(texto)))
-        .catch(trataErro);
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.blue(texto));
+    } catch(erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.green('Operação concluída!'));
+    }
 }
 
-// Um código síncrono
+// ** Uma das formas de lidar com assincronicidade em Javascript **
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then(texto => console.log(chalk.green(texto)))
+//         .catch(trataErro);
+// }
+
+// ** Um código síncrono **
+
 // function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8';
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
@@ -27,3 +42,4 @@ function pegaArquivo(caminhoDoArquivo) {
 // }
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
