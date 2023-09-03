18c0caf6a1affb050dd7532266585bd0c2ecc2e0
Yago <yago.pereira@estudante.ufjf.br>
2023-09-03 15:49:44 -0300

    Expressões regulares

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
+
+function extraiLinks(texto) {
+    const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = regex.exec(texto);
+    console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
@@ -15,7 +25,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     } catch(erro) {
         trataErro(erro);
     } finally {
-        console.log(chalk.green('Operação concluída!'));
+        console.log(chalk.yellow('Operação concluída!'));
     }
 }
 
@@ -43,3 +53,5 @@ async function pegaArquivo(caminhoDoArquivo) {
 
 pegaArquivo('./arquivos/texto.md');
 pegaArquivo('./arquivos/');
+
+// \[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
