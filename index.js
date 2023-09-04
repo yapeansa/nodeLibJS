a5052f4c03ac692768d849dd60a2a4217f9627ec
Yago <yago.pereira@estudante.ufjf.br>
2023-09-03 23:26:02 -0300

    Capturando links

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
-
 function extraiLinks(texto) {
     const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = regex.exec(texto);
-    console.log(capturas);
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+    return resultados;
 }
-
-extraiLinks(textoTeste);
 
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -21,13 +18,15 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.blue(texto));
+        console.log(extraiLinks(texto));
     } catch(erro) {
         trataErro(erro);
     } finally {
         console.log(chalk.yellow('Operação concluída!'));
     }
 }
+
+pegaArquivo('./arquivos/texto.md');
 
 // ** Uma das formas de lidar com assincronicidade em Javascript **
 
@@ -50,8 +49,5 @@ async function pegaArquivo(caminhoDoArquivo) {
 //         console.log(chalk.green(texto));
 //     });
 // }
-
-pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
 
 // \[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
