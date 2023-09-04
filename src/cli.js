81c21197574ea0c33600b020edee7b465942a6ed
Yago <yago.pereira@estudante.ufjf.br>
2023-09-04 08:14:00 -0300

    Usando a linha de comando.

---
+++ src/cli.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArquivo from "./index.js";
+
+const caminho = process.argv;
+
+function imprimeLista(resultado) {
+    console.log(chalk.yellow('Lista de links:'), resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(resultado);
+    }
+    else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista);
+        });
+    }
+}
+
+processaTexto(caminho);
+
+// executamos o arquivo a partir da raiz com 'node src/cli.js ./arquivos/texto.md'
