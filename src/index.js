ec246598ac9e674e2ae4c47e2ac2160ef6c6f9ae
Yago <yago.pereira@estudante.ufjf.br>
2023-09-06 08:27:23 -0300

    Ajustes finais.

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-// async/await
-
 async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
@@ -27,27 +25,3 @@ async function pegaArquivo(caminhoDoArquivo) {
 }
 
 export default pegaArquivo;
-
-// ** Uma das formas de lidar com assincronicidade em Javascript **
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then(texto => console.log(chalk.green(texto)))
-//         .catch(trataErro);
-// }
-
-// ** Um código síncrono **
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if (erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     });
-// }
-
-// \[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
